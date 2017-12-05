c48fca8dc84ce9c2c02e02e1b3c0c93833032b69
yuvii28 <yuvraj28singh@hotmail.com>
2017-12-05 14:18:39 +1100

    Day 3, part 2 complete

--- Day3/Spiral2.cpp
+++ Day3/Spiral2.cpp
@@ -3,15 +3,73 @@
 int main(int argc, char* argv[]){	
 	int spiral [501][501];
 	spiral[250][250] = 1;
-	int i = 250;
+	spiral[250][251] = 1;
+	spiral[249][251] = 2;
+	int i = 249;
 	int j = 250;
+
+	bool pass = false;
+	int numFill = 2;
+	int toFill = 2;
+
 	//How to move
-	char nextMove = 'r';
-	//marks how many times, after two change direction
-	int changeMove = 1;
+	char nextMove = 'l';
+
 	while (true){
+		//Fill box
+		spiral[i][j] = spiral[i+1][j-1] + spiral[i+1][j] + spiral[i+1][j+1] + 
+		    spiral[i][j-1] + spiral[i][j+1] + spiral[i-1][j-1] + spiral[i-1][j] +
+		     spiral[i-1][j+1];
+
+		if (spiral[i][j] > 368078){
+			break;
+		}
+
+		toFill--;
 		
-		break;
+		//Subtract number. If last, 		
+		if (toFill == 0){
+			if (pass){
+				numFill++;
+				pass = false;
+				toFill = numFill;
+			} else {
+				pass = true;
+				toFill = numFill;				
+			}
+
+			if (nextMove == 'u'){
+				nextMove = 'l';
+			}
+			else if (nextMove == 'l'){
+				nextMove = 'd';
+			}
+			else if (nextMove == 'd'){
+				nextMove = 'r';
+			} 
+			else {
+				nextMove = 'u';
+			}
+		}
+
+		//Move in direction
+		if (nextMove == 'u'){
+			i -= 1;
+		}
+		else if (nextMove == 'd'){
+			i += 1;
+		}
+		else if (nextMove == 'r'){
+			j += 1;
+		} 
+		else {
+			j -= 1;
+		}
+
 	}
+	std::cout << spiral[i][j] << "\n";
 	return 0;
 }
+
+
+
--- Day4/passphrase2.cpp
+++ Day4/passphrase2.cpp
@@ -35,3 +35,4 @@ int main(int argc, char* argv[]){
 	std::cout << result << "\n";
 	return 0;
 }
+
